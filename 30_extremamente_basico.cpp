c275998718c6f4e3042922b7068c3ab72848f1c7
FBR4Z <162244048+FBR4Z@users.noreply.github.com>
2024-04-04 19:06:51 -0400

    Create extremamente básico

---
+++ 30_extremamente_basico.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, X;
+
+    // Lê os valores de A e B
+    cin >> A >> B;
+
+    // Soma os valores de A e B
+    X = A + B;
+
+    // Imprime o resultado no formato especificado
+    cout << "X = " << X << endl;
+
+    return 0;
+}
