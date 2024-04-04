5158de9ad596daf92ce54a44cc1248a7329dd05c
FBR4Z <162244048+FBR4Z@users.noreply.github.com>
2024-04-04 18:59:26 -0400

    Fibonacci modified

--- 06_fibonacci.cpp
+++ 06_fibonacci.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// Função recursiva para calcular o número de Fibonacci
-unsigned long long fibonacci(int n) {
+// Função para calcular o número de Fibonacci
+unsigned long long fibonacci(int n, vector<unsigned long long>& memo) {
     if (n <= 1)
         return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    
+    if (memo[n] != 0)
+        return memo[n];
+
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
 }
 
 int main() {
     int T;
     cin >> T;
 
+    vector<unsigned long long> memo(61, 0); // Memoization para armazenar valores já calculados
+
     for (int i = 0; i < T; ++i) {
         int N;
         cin >> N;
-        cout << "Fib(" << N << ") = " << fibonacci(N) << endl;
+        cout << "Fib(" << N << ") = " << fibonacci(N, memo) << endl;
     }
 
     return 0;
