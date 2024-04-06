7e64c312a5e281df8818904a656631cf45c1a211
FBR4Z <162244048+FBR4Z@users.noreply.github.com>
2024-04-05 21:05:54 -0400

    Desespero

--- 19_area_circulo.js
+++ 19_area_circulo.js
@@ -4,9 +4,9 @@ const readline = require('readline').createInterface({
 });
 
 readline.question('', (raio) => {
-  const PI = 3.141592653589;
+  const PI = 3.14159;
   // raio = raio.toFixed(5)
-  const area = PI.toFixed(6) * parseFloat(raio) * parseFloat(raio);
+  const area = PI * parseFloat(raio) * parseFloat(raio);
   console.log(`A=${area.toFixed(4)}`);
   readline.close();
 });
