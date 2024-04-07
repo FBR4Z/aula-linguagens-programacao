cca2957e81f2f26cca1074b1786a2d70426cc8a3
FBR4Z <162244048+FBR4Z@users.noreply.github.com>
2024-04-07 10:21:22 -0400

    Update 01_lista2_bin.py

--- 01_lista2_bin.py
+++ 01_lista2_bin.py
@@ -6,11 +6,22 @@ def contar_1s_binarios(numero):
     return quantidade_1s
 
 def main():
-    # Solicita que o usuário insira um número decimal
-    numero_decimal = int(input("Digite um número decimal: "))
-    # Chama a função para contar os '1s' na forma binária do número e exibe o resultado
-    quantidade_1s = contar_1s_binarios(numero_decimal)
-    print("Quantidade de '1s' na forma binária:", quantidade_1s)
+    # Solicita que o usuário insira a quantidade de números
+    #quantidade_numeros = int(input("Digite a quantidade de números a serem testados: "))
+    quantidade_numeros = int(input())
+    numeros = []
+
+    # Solicita que o usuário insira os números
+    for i in range(quantidade_numeros):
+        #numero = int(input(f"Digite o {i + 1}º número: "))
+        numero = int(input())
+        numeros.append(numero)
+
+    # Calcula a quantidade de '1s' na forma binária para cada número
+    for i, numero in enumerate(numeros):
+        quantidade_1s = contar_1s_binarios(numero)
+        #print(f"Quantidade de '1s' na forma binária do {i + 1}º número:", quantidade_1s)
+        print(quantidade_1s)
 
 if __name__ == "__main__":
     main()
