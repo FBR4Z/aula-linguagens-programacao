500585e535f1949e038acbe8fa9cb12e079a27d1
FBR4Z <162244048+FBR4Z@users.noreply.github.com>
2024-04-07 20:44:05 -0400

    Update 10_lista2_atasadinhos.py

--- 10_lista2_atasadinhos.py
+++ 10_lista2_atasadinhos.py
@@ -6,13 +6,15 @@ def main():
     horarios_chegada = list(map(int, input().split()))
     
     # Contar quantos alunos chegaram atrasados
-    alunos_atrasados = sum(1 for horario in horarios_chegada if horario > 0)
+    alunos_presentes = sum(1 for horario in horarios_chegada if horario <= 0)
     
     # Verificar se o número mínimo de pessoas foi alcançado e se não há alunos atrasados
-    if alunos_atrasados > 0 or N - alunos_atrasados < K:
-        print("NO")
-    else:
+    if alunos_presentes >= K:
         print("YES")
+    else:
+        print("NO")
+
+    # print(alunos_presentes)
 
 if __name__ == "__main__":
     main()
